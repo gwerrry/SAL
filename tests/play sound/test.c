604c268095778f44d9b79cb261c289cd68b4e7fb
gwerrry <46902638+gwerrry@users.noreply.github.com>
2023-11-06 16:52:42 -0500

    Start on wav parser. This commit has bugs. This commit is for going from my laptop to my desktop.

--- tests/play sound/test.c
+++ tests/play sound/test.c
@@ -2,6 +2,27 @@
 
 int main(void) {
     sl_init();
-    printf("%d\n", leSys);
-    return 0;
+
+    printf("Is this system LITTLE endian... %s\n", leSys == 1 ? "yes" : "no");
+
+    printf("What WAVE file would you like to load?\n");
+
+    char *input = malloc(256 * sizeof(char));
+    if (input == NULL) {
+        printf("Error allocating memory!\n");
+        return -1;
+    }
+
+    fgets(input, 256, stdin);
+
+    SL_WAV_FILE* buf = NULL;
+    SLenum out = sl_parse_wave_file(input, buf);
+    if (buf) {
+        SLuchar *chunkId = buf->descriptorChunk.chunkID;
+        SLuint chunkSize = buf->descriptorChunk.chunkSize;
+        printf("Chunk Descriptor ID: %c%c%c%c\n", chunkId[0], chunkId[1], chunkId[2], chunkId[3]);
+        printf("Chunk size: %d\n", chunkSize);
+    }
+return out;
+
 }
