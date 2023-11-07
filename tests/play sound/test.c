839478012d8a5426a02ea09e6c2f09f8e12afcea
gwerrry <46902638+gwerrry@users.noreply.github.com>
2023-11-07 01:11:18 -0500

    Add Wave File Parsing

--- tests/play sound/test.c
+++ tests/play sound/test.c
@@ -16,14 +16,10 @@ int main(void) {
     fgets(input, 256, stdin);
     input[strcspn(input, "\n")] = 0;
 
-    SL_WAV_FILE* buf = NULL;
-    SLenum out = sl_parse_wave_file(input, buf);
-    if (buf) {
-        SLuchar *chunkId = buf->descriptorChunk.chunkID;
-        SLuint chunkSize = buf->descriptorChunk.chunkSize;
-        printf("Chunk Descriptor ID: %c%c%c%c\n", chunkId[0], chunkId[1], chunkId[2], chunkId[3]);
-        printf("Chunk size: %d\n", chunkSize);
-    }
+    SL_WAV_FILE buf;
+    SLenum out = sl_parse_wave_file(input, &buf);
+
+
 
     return out;
 }
