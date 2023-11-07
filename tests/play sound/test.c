d040ef7fab2d5424170a7f3343bd4aa2a400d617
gwerrry <46902638+gwerrry@users.noreply.github.com>
2023-11-07 02:18:39 -0500

    Fix buffer and add way to Free wave files.

--- tests/play sound/test.c
+++ tests/play sound/test.c
@@ -1,5 +1,8 @@
 #include "../../include/sal.h"
 
+#include <windows.h>
+#include <audioclient.h>
+
 int main(void) {
     sl_init();
 
@@ -16,10 +19,15 @@ int main(void) {
     fgets(input, 256, stdin);
     input[strcspn(input, "\n")] = 0;
 
-    SL_WAV_FILE buf;
+    SL_WAV_FILE* buf = NULL;
     SLenum out = sl_parse_wave_file(input, &buf);
 
-
+    if(out == SL_SUCCESS && buf) {
+        printf("Successfully parsed WAVE file at \"%s\".\n", input);
+        sl_free_wave_file(&buf);
+    } else {
+        printf("Failed to parse WAVE file at \"%s\".\n", input);
+    }
 
     return out;
 }
