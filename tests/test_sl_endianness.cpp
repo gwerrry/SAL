521e5073d89f5a74f2d9e045b2db9edea09c1fac
gwerrry <46902638+gwerrry@users.noreply.github.com>
2024-05-02 06:08:12 +0000

    commit testing structure

---
+++ tests/test_sl_endianness.cpp
@@ -0,0 +1,21 @@
+#include <sal.h>
+#include <bit>
+#include <cassert>
+
+void test_sl_get_native_endianness() {
+    // Determine the expected endianness
+    SL_ENDIANNESS expected;
+    if constexpr (std::endian::native == std::endian::little)
+        expected = SL_LITTLE_ENDIAN;
+    else if constexpr (std::endian::native == std::endian::big)
+        expected = SL_BIG_ENDIAN;
+
+    SL_ENDIANNESS actual = sl_get_native_endianness();
+
+    assert(actual == expected);
+}
+
+int main(void) {
+    test_sl_get_native_endianness();
+    return 0;
+}
