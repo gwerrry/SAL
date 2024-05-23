ab80eaaa730fca647ec097746d1eff1dd127d118
gwerrry <46902638+gwerrry@users.noreply.github.com>
2024-05-23 01:38:57 -0400

    sl buf to native uint test

--- tests/test_sl_endianness.cpp
+++ tests/test_sl_endianness.cpp
@@ -3,19 +3,19 @@
 #include <cassert>
 
 void test_sl_get_native_endianness() {
-    // Determine the expected endianness
-    SL_ENDIANNESS expected;
-    if constexpr (std::endian::native == std::endian::little)
-        expected = SL_LITTLE_ENDIAN;
-    else if constexpr (std::endian::native == std::endian::big)
-        expected = SL_BIG_ENDIAN;
+  // Determine the expected endianness
+  SL_ENDIANNESS expected;
+  if constexpr(std::endian::native == std::endian::little)
+    expected = SL_LITTLE_ENDIAN;
+  else
+    expected = SL_BIG_ENDIAN;
 
-    SL_ENDIANNESS actual = sl_get_native_endianness();
+  SL_ENDIANNESS actual = sl_get_native_endianness();
 
-    assert(actual == expected);
+  assert(actual == expected);
 }
 
 int main(void) {
-    test_sl_get_native_endianness();
-    return 0;
+  test_sl_get_native_endianness();
+  return 0;
 }
